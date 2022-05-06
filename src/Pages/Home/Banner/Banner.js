1239fa6f6965b7bad5269282b683fb35f9c44202
diptasahacse <diptasaha.lpu.cse@gmail.com>
2022-05-07 00:50:35 +0600

    add carousel as banner

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -1,10 +1,34 @@
 import React from 'react';
+import { Carousel } from 'react-bootstrap';
+import banner1 from '../../../images/banner/banner1.jpg'
+import banner2 from '../../../images/banner/banner2.jpg'
+import banner3 from '../../../images/banner/banner3.jpg'
 
 const Banner = () => {
     return (
-        <div>
-            
-        </div>
+        <Carousel fade>
+            <Carousel.Item>
+                <img
+                    className="d-block w-100 vh-100"
+                    src={banner1}
+                    alt="First slide"
+                />
+            </Carousel.Item>
+            <Carousel.Item>
+                <img
+                    className="d-block w-100  vh-100"
+                    src={banner2}
+                    alt="Second slide"
+                />
+            </Carousel.Item>
+            <Carousel.Item>
+                <img
+                    className="d-block w-100  vh-100"
+                    src={banner3}
+                    alt="Third slide"
+                />
+            </Carousel.Item>
+        </Carousel>
     );
 };
 
