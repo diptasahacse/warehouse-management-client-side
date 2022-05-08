ec8e5b535a5a9c958524d7f4629b4b8e7c33df4c
diptasahacse <diptasaha.lpu.cse@gmail.com>
2022-05-08 16:30:05 +0600

    implement google auth

--- src/Pages/Shared/SocialLogin/SocialLogin.js
+++ src/Pages/Shared/SocialLogin/SocialLogin.js
@@ -1,20 +1,55 @@
 import React from 'react';
+import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import { useLocation, useNavigate } from 'react-router-dom';
+import auth from '../../../firebase.init';
+import Loading from '../Loading/Loading';
 import SectionTitle from '../SectionTitle/SectionTitle';
 
 const SocialLogin = () => {
+
+    const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+
+    let navigate = useNavigate();
+    let location = useLocation();
+    let errorMessage;
+    let from = location.state?.from?.pathname || "/";
+
+
+
+    const signInWithGoogleHandler = () => {
+        signInWithGoogle();
+
+    }
+    if (loading) {
+        return <Loading></Loading>
+
+    }
+    if (user) {
+        navigate(from, { replace: true });
+
+    }
+    if (error) {
+        errorMessage = error?.message
+    }
     return (
         <div>
 
             <SectionTitle title='Sign in with' color='#212529'></SectionTitle>
             <div className='d-flex justify-content-center m-2'>
-                <div role='button' style={{ height: "40px", width: "40px", border:"1px solid #86BA09" }} className=' rounded-circle p-1 m-1'>
-                    <img className='img-fluid' src='https://i.ibb.co/1ZqJvmW/google.png' alt="Google" />
+                
+                <button onClick={signInWithGoogleHandler} className='  p-1 m-1 border-0 bg-transparent'>
+                    <div style={{ height: "40px", width: "40px", border: "1px solid #86BA09" }} className='rounded-circle'>
+                        <img className='img-fluid' src='https://i.ibb.co/1ZqJvmW/google.png' alt="Google" />
 
-                </div>
-                <div role='button' style={{ height: "40px", width: "40px", border:"1px solid #86BA09" }} className=' rounded-circle p-1 m-1'>
+                    </div>
+                </button>
+                <div style={{ height: "40px", width: "40px", border: "1px solid #86BA09" }} className=' rounded-circle p-1 m-1'>
                     <img className='img-fluid' src='https://i.ibb.co/KW4rvpJ/facebook.png' alt="Google" />
 
                 </div>
+            </div>
+            <div className='text-center'>
+                <p className='text-danger'>{errorMessage ? errorMessage : ''}</p>
             </div>
 
         </div>
